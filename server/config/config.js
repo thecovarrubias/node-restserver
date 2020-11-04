093a51a5e96a6739b5dfb6e05fa6fa55601ca8cf
thecovarrubias <thcovarrubias@gmail.com>
2020-11-04 16:08:40 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://pepe:wC2P9VXcaDzsshuw@cluster0.mjsvv.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
